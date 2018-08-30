5de3bec8d4d694bb6d1878ed5f96b1be3181eea5
ahlove3 <ahloveiii@gmail.com>
2018-08-30 16:29:42 -0400

    Add files via upload

--- Lab 1 Notebook.py
+++ Lab 1 Notebook.py
@@ -26,3 +26,25 @@ a = 3
 
 print (a)
 
+
+# In[3]:
+
+
+get_ipython().system('pip install xlrd')
+
+
+# In[4]:
+
+
+import xlrd
+book = xlrd.open_workbook("Diamonds.xls")
+sheet = book.sheet_by_name("Diamonds")
+
+
+# In[5]:
+
+
+for row_index in range(1,5): # read the first 4 rows, skip the first row
+    id_, weight, color,_,_,price = sheet.row_values(row_index)
+    print(id_,weight,color,price)
+
